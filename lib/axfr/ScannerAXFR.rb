7e04e38d744f7cbb765a654c81d0c35db2c7d81c
maximusfox <shok@cpan.org>
2016-08-11 13:21:34 +0300

    AXFR scanner FIX Add single thread run method Add callbacks

--- lib/axfr/ScannerAXFR.rb
+++ lib/axfr/ScannerAXFR.rb
@@ -1,11 +1,17 @@
 require 'net/dns'
 
 class ScannerAXFR
-  def initialize(target_list, threads=5)
+  def initialize(target_list, threads = 5)
     return nil if target_list.nil?
     @target_list = target_list
 
+    return nil if threads <= 0
     @threads = threads
+
+    @callbacks = {
+        :FOUND => self.method(:defaultCallback_FOUND),
+        :ON_TARGET_FINISH => self.method(:defaultCallback_ON_TARGET_FINISH),
+    }
   end
 
   def resolverGetInfo(target, resolver = Net::DNS::Resolver.new)
@@ -23,7 +29,9 @@ class ScannerAXFR
       begin
         netDNS.axfr(target).answer.each do |answer|
           if answer.respond_to?('type') and answer.type == 'A'
-            result.push(answer.name.chomp('.'))
+            subdomain = answer.name.chomp('.')
+            result.push(subdomain)
+            @callbacks[:FOUND].call(target, subdomain)
           end
         end
       rescue
@@ -52,8 +60,7 @@ class ScannerAXFR
           end
 
           results.delete(target)
-          puts "[Resolver] Target: #{target} Resolved: #{results.size}"
-          # TODO: событие ONE_TARGET_FINISH axfr
+          @callbacks[:ON_TARGET_FINISH].call(target, results)
 
           found_domains[target] = results
         end
@@ -65,15 +72,60 @@ class ScannerAXFR
     found_domains
   end
 
+  def run_without_threads
+    found_domains = {}
+
+    while @target_list.size > 0
+      target = @target_list.pop
+
+      begin
+        results = resolverGetInfo(target)
+      rescue
+        results = []
+      end
+
+      results.delete(target)
+      @callbacks[:ON_TARGET_FINISH].call(target, results)
+
+      found_domains[target] = results
+    end
+
+    found_domains
+  end
+
+  def defaultCallback_ON_TARGET_FINISH(target, results)
+    puts "[Results] Target: #{target} Resolved: #{results.size}"
+  end
+
+  def defaultCallback_FOUND(target, subdomain)
+    puts "[FOUND] #{subdomain}"
+  end
+
+  def setCallback_FOUND(&block)
+    if block.respond_to?('call')
+      @callbacks[:FOUND] = block
+      return true
+    end
+
+    false
+  end
+
+  def setCallback_ON_TARGET_FINISH(&block)
+    if block.respond_to?('call')
+      @callbacks[:ON_TARGET_FINISH] = block
+      return true
+    end
+
+    false
+  end
+
 end
 
 if $0 == __FILE__
-  puts '[INFO] Start AXFR resolver ... [ OK ]'
   brt = ScannerAXFR.new(
-      target_list = ['merron.ru', 'mr-ozio.ru'],
+      target_list = ['merron.ru', 'mr-ozio.ru', 'text.ru', 'nn.ru'],
       threads=1,
   )
-  puts '[INFO] Finish AXFR resolver ... [ OK ]'
 
   p brt.run
 end
