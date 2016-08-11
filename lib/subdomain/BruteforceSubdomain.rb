6aee0846fdc40fdde54e0bfeb4ce430028c05c4b
maximusfox <shok@cpan.org>
2016-08-11 06:07:20 +0300

    Subdomain bruteforce prototype

---
+++ lib/subdomain/BruteforceSubdomain.rb
@@ -0,0 +1,117 @@
+require 'net/dns'
+require 'httpclient'
+
+class BruteforceSubdomain
+  def initialize(target_list, dictionary = [], threads=5, check_web_responses = false, respose_codes = [200])
+    return nil if target_list.nil?
+    @target_list = target_list
+
+    return nil if dictionary.nil?
+    @dictionary = dictionary
+
+    @threads = threads
+    return nil if threads <= 0
+
+    if check_web_responses
+      @ua = HTTPClient.new
+      @checkWebResponses = check_web_responses
+      @resposeCodes = respose_codes
+    end
+
+  end
+
+  def resolv(target, resolver = Net::DNS::Resolver)
+    resolver.start(target).answer.each do |answer|
+      return true if answer.respond_to?('type') and answer.type == 'A'
+    end
+
+    false
+  end
+
+  def checkStatusHttp(target, ua = @ua)
+    begin
+      resp_http = ua.head("http://#{target}")
+    rescue
+
+    end
+
+    if resp_http.respond_to?('status') and @resposeCodes.include?(resp_http.status)
+      return true
+    end
+
+    false
+  end
+
+  def checkStatusHttps(target, ua = @ua)
+    begin
+      resp_https = ua.head("https://#{target}")
+    rescue
+
+    end
+
+    if resp_https.respond_to?('status') and @resposeCodes.include?(resp_https.status)
+      return true
+    end
+
+    false
+  end
+
+
+  def run
+    found_domains = {}
+
+    threads_list = []
+    @threads.times do
+      threads_list << Thread.new do
+
+        while @target_list.size > 0
+          target = @target_list.pop
+
+          results = []
+          @dictionary.each do |subname|
+            subdomain = subname+'.'+target
+            if resolv(subdomain)
+              if @checkWebResponses
+                if (checkStatusHttp(subdomain) or checkStatusHttps(subdomain))
+                  results.push(subdomain)
+                  # puts "[FOUND] #{subdomain}"
+                  # TODO: событие FOUND subdomain
+                else
+                  puts "[NOT EXIST] #{subdomain}"
+                  # TODO: событие NOT_EXIST subdomain
+                end
+              else
+                results.push(subdomain)
+                # puts "[FOUND] #{subdomain}"
+                # TODO: событие FOUND subdomain
+              end
+            else
+              puts "[NOT EXIST] #{subdomain}"
+              # TODO: событие NOT_EXIST subdomain
+            end
+          end
+
+          puts "[Resolver] Target: #{target} Resolved: #{results.size}"
+          # TODO: событие ONE_TARGET_FINISH subdomain
+
+          found_domains[target] = results
+        end
+
+      end
+    end
+
+    threads_list.each {|thr| thr.join }
+    found_domains
+  end
+end
+
+if $0 == __FILE__
+  brt = BruteforceSubdomain.new(
+      target_list = ['merron.ru', 'mr-ozio.ru'],
+      dictionary = ['shok', 'test', 'azaza', 'lalka', 'pidor'],
+      threads=1,
+      check_web_responses=true
+  )
+
+  p brt.run
+end
